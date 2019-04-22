e9632609c4a831c4ca647c290cc60c748e2a0597
ryanolee <rol@bluetel.co.uk>
2019-04-22 22:41:43 +0100

    Fix date time offeset bug

--- providers/togglProvider.js
+++ providers/togglProvider.js
@@ -18,9 +18,9 @@ class TogglProvider{
         let toReturn = [];
         since.setHours(0,0,0,0);
         for(let i = 0; i <= days; i++){
-            
-            let targetSince = new Date(since.getTime() - ((i+1) * 24 * 60 * 60 * 1000));
-            let targetUntil = new Date(since.getTime() - (i * 24 * 60 * 60 * 1000) - 1000);
+            //Get target date by (now - days * 24 hours - datetimeoffset)
+            let targetSince = new Date(since.getTime() - ((i+1) * 24 * 60 * 60 * 1000) - (since.getTimezoneOffset() * 60 * 1000));
+            let targetUntil = new Date(since.getTime() - (i * 24 * 60 * 60 * 1000) - (since.getTimezoneOffset() * 60 * 1000) - 1000);
             console.log(targetSince, targetUntil);
             let result = await this.client.getReportData(this.workspaceID, targetSince, targetUntil);
             //throw new Error();
