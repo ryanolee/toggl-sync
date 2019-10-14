f3d247e22c45b0be97ab97a3a88218da1af12aff
Ryan Lee <rol@bluetel.co.uk>
2019-10-14 10:06:04 +0100

    Update Rounding code

--- transformations/days/timeRound.js
+++ transformations/days/timeRound.js
@@ -9,7 +9,7 @@ module.exports = {
              //Ms to hours conversion
             task.duration = task.duration / (1000 * 3600);
             //Round to nearest .25
-            task.duration = (Math.round(task.duration * 4) / 4).toFixed(2);
+            task.duration = Math.max((Math.round(task.duration * 4) / 4).toFixed(2), 0.25);
             //store rounded task time
             day[taskIndex] = task;
         }
